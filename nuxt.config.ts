4443f5123fd9ff9412e65d74817f7963a7cb89b2
Elliott Zhao <elliott.zhao@qq.com>
2024-09-14 13:15:52 +0800

    feat: 添加国际化支持
    - 新增 i18n.config.ts 文件，配置 Vue I18n
    - 更新 nuxt.config.ts，集成 @nuxtjs/i18n 模块
    - 更新 package.json 和 package-lock.json，添加相关依赖

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@vueuse/nuxt', '@nuxtjs/i18n'],
+  modules: ['@vueuse/nuxt', '@nuxtjs/i18n', '@ant-design-vue/nuxt'],
+  i18n: {
+    vueI18n: './i18n.config.ts'
+  },
+  antd: {
+  }
 })
